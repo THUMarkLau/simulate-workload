eff2241910c932f1bcabf9e65fa9dcfed816a4cb
Xiangpeng Hu <hxp23@mails.tsinghua.edu.cn>
2024-07-03 18:46:36 +0800

    setSchemaTemplate

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -147,7 +147,25 @@ public class Main {
     }
   }
 
+  private static void setSchemaTemplate() {
+    if (Configuration.mode.equalsIgnoreCase("iotdb")) {
+      try {
+        GlobalSessionPool.getInstance().executeNonQueryStatement("create database root.toyotads;");
+        GlobalSessionPool.getInstance()
+            .executeNonQueryStatement(
+                "create device template t1 (AccelerationFB DOUBLE encoding=GORILLA, AccelerationLR DOUBLE encoding=GORILLA, Speed_TypeA DOUBLE encoding=GORILLA, SteeringAngle_TypeA DOUBLE encoding=GORILLA, EngineRPM_TypeA DOUBLE encoding=GORILLA, TirePressureFL_kpa DOUBLE encoding=GORILLA, latitude DOUBLE encoding=GORILLA, longitude DOUBLE encoding=GORILLA, AccelPedalAngle_TypeA DOUBLE encoding=GORILLA, TirePressureFR_kpa DOUBLE encoding=GORILLA, TirePressureRL_kpa DOUBLE encoding=GORILLA, TirePressureRR_kpa DOUBLE encoding=GORILLA, AmbientTemperature DOUBLE encoding=GORILLA, TemperatureD DOUBLE encoding=GORILLA, FuelGageIndication INT32 encoding=TS_2DIFF, TurnLampSwitchStatus INT32 encoding=TS_2DIFF, ATShiftPosition INT32 encoding=TS_2DIFF, BrakePedal INT32 encoding=TS_2DIFF, DoorOpenD INT32 encoding=TS_2DIFF, ParkingBrake INT32 encoding=TS_2DIFF, EcoModeIndicator INT32 encoding=TS_2DIFF, PowerModeSelect_TypeA INT32 encoding=TS_2DIFF, SportModeSelect INT32 encoding=TS_2DIFF, WindowPositionD INT32 encoding=TS_2DIFF, AirConIndicator INT32 encoding=TS_2DIFF, Odometer_km INT32 encoding=TS_2DIFF, HeadLamp_TypeB INT32 encoding=TS_2DIFF);");
+        GlobalSessionPool.getInstance()
+            .executeNonQueryStatement("set device template t1 to root.toyotads;");
+        logger.info("Set schema template successfully");
+      } catch (IoTDBConnectionException | StatementExecutionException e) {
+        // ignore
+      }
+    }
+  }
+
   private static void startLoadingCSV() {
+    setSchemaTemplate();
+
     int loadingThreadCount = csvDirectories.size();
     logger.info("Using {} threads to load CSV", loadingThreadCount);
     writeService =
